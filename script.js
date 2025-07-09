dcbceae9616b0c75abe6eafe56629f551d6e05e0
bhakti <bhaktiyadav98@gmail.com>
2025-07-09 18:16:38 +0530

    Code

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ navLinks.forEach(link => {
     navbar.classList.remove("active");
   });
 });
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw5stS_9HKkSw1jPS1EXwRX3ei2L1Q0RWUl9BqcPLCVpaIxqS94Pkdk2StHSNA6gc92tw/exec'; // 👈 Replace with your script URL
+  const form = document.getElementById('feedbackForm');
+  const responseMsg = document.getElementById('responseMsg');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    responseMsg.textContent = "Submitting...";
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        responseMsg.textContent = "✅ Thank you! Your feedback has been submitted.";
+        form.reset();
+      })
+      .catch(error => {
+        console.error('Error!', error.message);
+        responseMsg.textContent = "❌ There was an error. Please try again.";
+        responseMsg.style.color = "red";
+      });
+  });
